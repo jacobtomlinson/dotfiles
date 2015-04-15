166ab9cc9a955abc0b90fa114122091837af811a
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2015-04-15 21:16:15 +0100

    Added instructions and variables.
    This should make it easier for other people to use and customise.

--- boxen.pp
+++ boxen.pp
@@ -1,13 +1,23 @@
-  $dotfiles_dir = "${boxen::config::srcdir}/dotfiles"
+# Dotfiles GitHub repository to install, if you've forked then change
+# this to be your repo.
+$dotfiles_repo = "jacobtomlinson/dotfiles"
 
-  repository { $dotfiles_dir:
-    source => "jacobtomlinson/dotfiles"
-  }
+# Directory to clone the dotfiles repo into, they will be symlinked
+# from here so it doesn't really matter where you choose.
+$dotfiles_dir = "${boxen::config::srcdir}/dotfiles"
 
-  exec { "install dotfiles":
-    cwd      => $dotfiles_dir,
-    command  => "./install.sh",
-    provider => shell,
-    creates  => "${home}/.bashrc",
-    require  => Repository[$dotfiles_dir]
-  }
+# Clone the dotfiles repo.
+repository { $dotfiles_dir:
+  source => $dotfiles_repo
+}
+
+# Execute the dotfiles installer unless .bashrc already exists, which it
+# will not on a Mac by default. If you want to run the installer again
+# just delete your .bashrc and run boxen.
+exec { "install dotfiles":
+  cwd      => $dotfiles_dir,
+  command  => "./install.sh",
+  provider => shell,
+  creates  => "${home}/.bashrc",
+  require  => Repository[$dotfiles_dir]
+}
