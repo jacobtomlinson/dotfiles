1cc160226d414e4fd4f8a44d5ec90ea29646caf7
Jacob Tomlinson <jtomlinson@nvidia.com>
2020-10-27 15:05:28 +0000

    Add new git main commands

--- .zshrc.d/git.zsh
+++ .zshrc.d/git.zsh
@@ -1,1 +1,16 @@
-alias glum="git pull upstream main"
+function gdb () {
+	REMOTE=${1:-upstream}
+	git fetch $REMOTE
+	git remote show $REMOTE | grep "HEAD branch" | sed 's/.*: //'
+}
+
+
+unalias gcm
+function gcm () {
+       git checkout $(gdb $1)
+}
+
+unalias glum
+function glum () {
+	git pull upstream $(gdb upstream)
+}
