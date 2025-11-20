fa55cb5e49d6b50412072a4ec48bde0eb051433e
Jacob Tomlinson <jacob@tomlinson.email>
2025-11-20 11:49:18 +0000

    Auto update git email based on username

--- .zshrc.d/git.zsh
+++ .zshrc.d/git.zsh
@@ -1,9 +1,20 @@
+# Set git email based on username
+if [ "$(whoami)" = "jacob" ]; then
+    email="jacob@tomlinson.email"
+elif [ "$(whoami)" = "jtomlinson" ]; then
+    email="jtomlinson@nvidia.com"
+fi
+
+if [ -n "$email" ]; then
+    sed -i.bak "s/^\([[:space:]]*email[[:space:]]*=[[:space:]]*\).*/\1$email/" ~/.gitconfig
+fi
+
+
 function gub () {
 	REMOTE=${1:-upstream}
 	git fetch $REMOTE
 	git remote show $REMOTE | grep "HEAD branch" | sed 's/.*: //'
 }
-
 
 unalias gcm
 function gcm () {
